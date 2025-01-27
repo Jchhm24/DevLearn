f2944d31870c5f88509bda385b0252239b787c4d
Jchhm24 <jcchm2004@gmail.com>
2025-01-26 23:23:22 -0600

    agregar interfaz Curso y datos de cursos disponibles

---
+++ src/data/constans/cursos.ts
@@ -0,0 +1,76 @@
+import { Curso } from "@/interfaces/curso";
+
+export const cursos: Curso[] = [
+    {
+        id: 1,
+        nombre: "Curso de React 19: Novedades esenciales",
+        descripcion:
+            "Domina las funcionalidades más recientes de React 19 y aprende cómo aprovecharlas para optimizar tus proyectos de desarrollo web.",
+        miniatura: "/src/assets/images/miniaturas/React-19.jpg",
+        duracion: "40 min",
+        precio: 200,
+        fecha: "2025-01-20",
+        autor: "Midudev",
+        fotoAutor: "/src/assets/images/profile.jpg",
+    },
+    {
+        id: 2,
+        nombre: "Curso de Vue JS: Desde lo básico hasta la maestría",
+        descripcion:
+            "Adéntrate en Vue JS desde lo más básico y avanza hasta convertirte en un desarrollador experto con prácticas y ejemplos reales.",
+        miniatura: "/src/assets/images/miniaturas/Vuejs.jpg",
+        duracion: "8 hr",
+        precio: 150,
+        fecha: "2025-01-10",
+        autor: "Midudev",
+        fotoAutor: "/src/assets/images/profile.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Crea una API REST profesional con NodeJS",
+        descripcion:
+            "Aprende a construir APIs REST desde cero utilizando NodeJS y Express, implementando las mejores prácticas de desarrollo backend.",
+        miniatura: "/src/assets/images/miniaturas/NestJS.jpg",
+        duracion: "35 min",
+        precio: 250,
+        fecha: "2025-01-08",
+        autor: "Midudev",
+        fotoAutor: "/src/assets/images/profile.jpg",
+    },
+    {
+        id: 4,
+        nombre: "Manipulación avanzada del DOM con JavaScript",
+        descripcion:
+            "Descubre cómo manejar dinámicamente el DOM para crear interfaces web interactivas y atractivas utilizando JavaScript.",
+        miniatura: "/src/assets/images/miniaturas/JavaScript.jpg",
+        duracion: "5 hr",
+        precio: 100,
+        fecha: "2025-01-05",
+        autor: "Midudev",
+        fotoAutor: "/src/assets/images/profile.jpg",
+    },
+    {
+        id: 5,
+        nombre: "Curso completo de CSS3: Principiante a experto",
+        descripcion:
+            "Domina CSS3 desde los conceptos básicos hasta técnicas avanzadas de diseño responsivo, animaciones y personalización visual.",
+        miniatura: "/src/assets/images/miniaturas/Css3.jpg",
+        duracion: "3 hr",
+        precio: 50,
+        fecha: "2025-01-03",
+        autor: "Midudev",
+        fotoAutor: "/src/assets/images/profile.jpg",
+    },
+    {
+        id: 6,
+        nombre: "Fundamentos de HTML5: Crea tu primera web",
+        descripcion:
+            "Aprende los fundamentos esenciales de HTML5 y adquiere las habilidades necesarias para construir páginas web estructuradas y accesibles.",
+        miniatura: "/src/assets/images/miniaturas/HTML5.jpg",
+        duracion: "1 hr",
+        precio: 20,
+        fecha: "2025-01-01",
+        autor: "Midudev",
+        fotoAutor: "/src/assets/images/profile.jpg",
+    },
+];
---
+++ src/interfaces/curso.ts
@@ -0,0 +1,11 @@
+export interface Curso {
+    id:number
+    nombre:string
+    descripcion:string
+    miniatura:string
+    duracion:string
+    precio:number
+    fecha: string
+    autor: string
+    fotoAutor: string
+}
